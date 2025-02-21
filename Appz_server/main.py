6946a626c583e9b7de3e80926647326f1ef023c9
TuanAnh <Anhconstant@gmail.com>
2025-02-21 19:38:45 +0700

    Create main.py

---
+++ Appz_server/main.py
@@ -0,0 +1,12 @@
+from machine import Pin
+import time
+time.sleep(2)
+ver = "1.0"
+print("main in ver "+ ver)
+print("start main")
+led = Pin(15,Pin.OUT)
+
+while True:
+    led.value(not(led.value()))
+    time.sleep(1)
+    
