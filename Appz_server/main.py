fc208023624a8ead00be6bfa938a4ad2eaf4083f
TuanAnh <Anhconstant@gmail.com>
2025-02-21 19:59:16 +0700

    Update main.py

--- Appz_server/main.py
+++ Appz_server/main.py
@@ -1,12 +1,12 @@
 from machine import Pin
 import time
 time.sleep(2)
-ver = "1.1"
+ver = "1.2"
 print("main in ver "+ ver)
 print("start main")
 led = Pin(15,Pin.OUT)
 
 while True:
     led.value(not(led.value()))
-    time.sleep_ms(100)
+    time.sleep_ms(200)
     
